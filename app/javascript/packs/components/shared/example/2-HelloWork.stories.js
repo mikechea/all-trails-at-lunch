9c6c838cacc9596bd6ff327c184304d677a89ffa
mikechea <cheamike@gmail.com>
2020-05-13 22:12:29 -0700

    setup storybook

---
+++ app/javascript/packs/components/shared/example/2-HelloWork.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+import Example from './'
+
+export default {
+  title: 'Example',
+  component: Example,
+};
+
+export const FirstExample = () => (
+  <Example />
+);
